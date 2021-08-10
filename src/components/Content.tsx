510b34c8aaf6bfc9f6f69348c655b34a191ce764
pedro-cal <pedroluna2@hotmail.com>
2021-08-10 11:19:48 -0300

    Finished

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,35 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from '../components/MovieCard';
+
+interface Movie {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  selectedGenre: { title: string };
+  movies: Array<Movie>;
+}
+
+export function Content(props: ContentProps) {
+  return (
+    <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {props.selectedGenre.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {props.movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  );
 }
